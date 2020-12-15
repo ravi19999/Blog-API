5b14b1b0f139782404d199682df3df54968a5ac6
ravi19999 <dahalravindra@gmail.com>
2020-12-15 23:46:52 +0545

    Deleted the desktop folder :sparkles:

--- blog/settings.py
+++ blog/settings.py
@@ -58,8 +58,13 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
+
 
 ROOT_URLCONF = 'blog.urls'
 
