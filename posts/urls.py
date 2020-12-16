d0bd8289500563998794173466e9a91db9a6b76c
ravi19999 <dahalravindra@gmail.com>
2020-12-16 13:55:03 +0545

    Added endpoints for showing registered users and their details :card_file_box:

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, UserList, UserDetail
 
 urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view()),
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
     path('', PostList.as_view()),
+    path('<int:pk>/', PostDetail.as_view()),
 ]
